0e0e01fbb4789e4df7debb410f3fc0834243bf4d
yash-vardhan-singh07 <150700501+yash-vardhan-singh07@users.noreply.github.com>
2026-01-16 02:44:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Shivam from "./pages/Shivam";
 import Gift from "./pages/Gift";
 import Praharsh from "./pages/Praharsh";
 import Claim from "./pages/Claim";
+import alag from "./pages/alag";
 
 function App() {
   return (
@@ -11,12 +12,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Yash />} />
         <Route path="/reward" element={<Praharsh />} />
-        <Route path="/rewards" element={<Shivam />} />
         <Route path="/gift" element={<Gift />} />
         <Route path="/claims" element={<Shivam />} />
         <Route path="/claim" element={<Shivam />} />
         <Route path="/amzn" element={<Shivam />} />
         <Route path="/claimgift" element={<Claim />} />
+        <Route path="/rewards" element={<alag />} />
       </Routes>
     </BrowserRouter>
   );
